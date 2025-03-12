04c3f4a489cca7df2727037c4ed0435cc2f2b8bc
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2025-03-12 11:25:26 -0700

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  gsap.set(["#img-2, #img-3, #img-4"], { top: "150%" });
+  gsap.set(["#gif-2, #gif-3, #gif-4"], { top: "150%" });
 
-  gsap.to(".menu-img img", {
-    top: "50%",
-    duration: 1.25,
-    ease: "power4.inOut",
-    stagger: 0.1,
-    delay: 0.25,
+  gsap.to(".menu-gif img", {
+      top: "50%",
+      duration: 1.25,
+      ease: "power4.inOut",
+      stagger: 0.1,
+      delay: 0.25,
   });
 });
--- js/tilt.js
+++ js/tilt.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const menuImgContainer = document.querySelector(".menu-img");
-  const images = document.querySelectorAll(".menu-img img");
+  const menuGifContainer = document.querySelector(".menu-gif");
+  const gifs = document.querySelectorAll(".menu-gif img");
   let mouse = { x: 0, y: 0 };
   let cx = window.innerWidth / 2;
   let cy = window.innerHeight / 2;
@@ -8,39 +8,39 @@ document.addEventListener("DOMContentLoaded", function () {
   const scales = [0.81, 0.84, 0.87, 0.9];
 
   function update() {
-    let dx = mouse.x - cx;
-    let dy = mouse.y - cy;
+      let dx = mouse.x - cx;
+      let dy = mouse.y - cy;
 
-    let tiltx = (dy / cy) * 20;
-    let tilty = (dx / cx) * 20;
+      let tiltx = (dy / cy) * 20;
+      let tilty = (dx / cx) * 20;
 
-    gsap.to(menuImgContainer, {
-      duration: 2,
-      transform: `rotate3d(${tiltx}, ${tilty}, 0, 15deg)`,
-      ease: "power3.out",
-    });
-
-    images.forEach((img, index) => {
-      let parallaxX = -(dx * (index + 1)) / 100;
-      let parallaxY = -(dy * (index + 1)) / 100;
-
-      let transformStyles = `translate(calc(-50% + ${parallaxX}px), calc(-50% + ${parallaxY}px)) scale(${scales[index]})`;
-      gsap.to(img, {
-        duration: 2,
-        transform: transformStyles,
-        ease: "power3.out",
+      gsap.to(menuGifContainer, {
+          duration: 2,
+          transform: `rotate3d(${tiltx}, ${tilty}, 0, 15deg)`,
+          ease: "power3.out",
       });
-    });
+
+      gifs.forEach((gif, index) => {
+          let parallaxX = -(dx * (index + 1)) / 100;
+          let parallaxY = -(dy * (index + 1)) / 100;
+
+          let transformStyles = `translate(calc(-50% + ${parallaxX}px), calc(-50% + ${parallaxY}px)) scale(${scales[index]})`;
+          gsap.to(gif, {
+              duration: 2,
+              transform: transformStyles,
+              ease: "power3.out",
+          });
+      });
   }
 
   document.body.addEventListener("mousemove", function (event) {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
-    update();
+      mouse.x = event.clientX;
+      mouse.y = event.clientY;
+      update();
   });
 
   window.addEventListener("resize", function () {
-    cx = window.innerWidth / 2;
-    cy = window.innerHeight / 2;
+      cx = window.innerWidth / 2;
+      cy = window.innerHeight / 2;
   });
 });
